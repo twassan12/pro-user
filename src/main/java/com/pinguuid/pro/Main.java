923308b82dcb9f7011cbb0c9d85be0c7a24e7cb6
pinguuid <pinguuid@gmail.com>
2020-10-03 17:24:32 +0300

    update same-library to 1.1 version

--- src/main/java/com/pinguuid/pro/Main.java
+++ src/main/java/com/pinguuid/pro/Main.java
@@ -6,7 +6,7 @@ import com.pinguuid.mi8.SameImpl;
 public class Main {
   public static void main(String[] args) {
     SameAPI api = new SameImpl();
-    String initialized = api.init(System.currentTimeMillis(), "for testing purpose");
+    String initialized = api.initialize(System.currentTimeMillis(), "for testing purpose");
     System.out.println("library was initialized: " + initialized);
   }
 }
